fc38a10029f5c7a2c16ac872db4622f7569a1d74
Ivriel <gunawanivriel@gmail.com>
2025-10-08 14:15:32 +0700

    modif interceptor agar langsung tendang ke login jika udah 401 requestnya

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,11 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { isPlatformBrowser } from '@angular/common';
 import { inject, PLATFORM_ID } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import Swal from 'sweetalert2';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const platformId = inject(PLATFORM_ID);
-  const cookieService = inject(CookieService)
+  const cookieService = inject(CookieService);
+  const router = inject(Router);
   
   const token = isPlatformBrowser(platformId) ? cookieService.get("token") : null;
 
@@ -19,7 +23,24 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(newReq);
+    return next(newReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          cookieService.delete('token', '/');
+          cookieService.delete('userData', '/');
+          
+          Swal.fire({
+            icon: 'error',
+            title: 'Session Expired',
+            text: 'Your session has expired. Please login again.',
+            confirmButtonText: 'OK'
+          }).then(() => {
+            router.navigateByUrl('/admin/login');
+          });
+        }
+        return throwError(() => error);
+      })
+    );
   }
 
   return next(req);
