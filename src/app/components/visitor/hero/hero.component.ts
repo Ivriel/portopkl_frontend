5be7b9308add5484a1d31720e2393f6865ef4e07
Ivriel <gunawanivriel@gmail.com>
2025-10-09 15:52:17 +0700

    bikin section hero dan matikan ssr

--- src/app/components/visitor/hero/hero.component.ts
+++ src/app/components/visitor/hero/hero.component.ts
@@ -1,13 +1,21 @@
+// hero.component.ts
 import { Component } from '@angular/core';
-import { NgxGalaxyComponent } from "@omnedia/ngx-galaxy";
+import { CommonModule } from '@angular/common';
 import { NgxTypewriterComponent } from '@omnedia/ngx-typewriter';
 
 @Component({
   selector: 'app-hero',
-  imports: [NgxGalaxyComponent,NgxTypewriterComponent],
+  imports: [CommonModule, NgxTypewriterComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-  words:string[] = ['Hello','Welcome to','Porto PKL']
+  words: string[] = ['Hello there', 'Welcome to', 'Porto PKL'];
+  
+  scrollToContent(): void {
+    const element = document.getElementById('content');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
